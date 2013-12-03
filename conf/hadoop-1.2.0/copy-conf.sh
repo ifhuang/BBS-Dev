c38086b082618bf25b124cc619dee056f33dc7f6
If <ifhuang91@gmail.com>
2013-12-04 03:53:47 +0800

    backup hadoop conf

---
+++ conf/hadoop-1.2.0/copy-conf.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+scp -r * hadoop-4:/usr/yifu/hadoop-1.2.1/conf/
+scp -r * hadoop-5:/usr/yifu/hadoop-1.2.1/conf/
+scp -r * hadoop-6:/usr/yifu/hadoop-1.2.1/conf/
+scp -r * hadoop-7:/usr/yifu/hadoop-1.2.1/conf/
+scp -r * hadoop-9:/usr/yifu/hadoop-1.2.1/conf/
+scp -r * hadoop-10:/usr/yifu/hadoop-1.2.1/conf/
+scp -r * hadoop-11:/usr/yifu/hadoop-1.2.1/conf/
+scp -r * hadoop-12:/usr/yifu/hadoop-1.2.1/conf/
+scp -r * hadoop-13:/usr/yifu/hadoop-1.2.1/conf/
+scp -r * hadoop-14:/usr/yifu/hadoop-1.2.1/conf/
+scp -r * hadoop-15:/usr/yifu/hadoop-1.2.1/conf/
+scp -r * hadoop-16:/usr/yifu/hadoop-1.2.1/conf/
+scp -r * hadoop-19:/usr/yifu/hadoop-1.2.1/conf/
+scp -r * hadoop-20:/usr/yifu/hadoop-1.2.1/conf/
+scp -r * hadoop-21:/usr/yifu/hadoop-1.2.1/conf/
+scp -r * hadoop-22:/usr/yifu/hadoop-1.2.1/conf/
+scp -r * hadoop-23:/usr/yifu/hadoop-1.2.1/conf/
+scp -r * hadoop-24:/usr/yifu/hadoop-1.2.1/conf/
+scp -r * hadoop-25:/usr/yifu/hadoop-1.2.1/conf/
+scp -r * hadoop-26:/usr/yifu/hadoop-1.2.1/conf/
+scp -r * hadoop-27:/usr/yifu/hadoop-1.2.1/conf/
+scp -r * hadoop-28:/usr/yifu/hadoop-1.2.1/conf/
+scp -r * hadoop-29:/usr/yifu/hadoop-1.2.1/conf/
+scp -r * hadoop-32:/usr/yifu/hadoop-1.2.1/conf/
---
+++ conf/hadoop-1.2.0/shutdown-all.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+ssh root@hadoop-4 shutdown now
+ssh root@hadoop-5 shutdown now
+ssh root@hadoop-6 shutdown now
+ssh root@hadoop-7 shutdown now
+ssh root@hadoop-9 shutdown now
+ssh root@hadoop-10 shutdown now
+ssh root@hadoop-11 shutdown now
+ssh root@hadoop-12 shutdown now
+ssh root@hadoop-13 shutdown now
+ssh root@hadoop-14 shutdown now
+ssh root@hadoop-15 shutdown now
+ssh root@hadoop-16 shutdown now
+ssh root@hadoop-19 shutdown now
+ssh root@hadoop-20 shutdown now
+ssh root@hadoop-21 shutdown now
+ssh root@hadoop-22 shutdown now
+ssh root@hadoop-23 shutdown now
+ssh root@hadoop-24 shutdown now
+ssh root@hadoop-25 shutdown now
+ssh root@hadoop-26 shutdown now
+ssh root@hadoop-27 shutdown now
+ssh root@hadoop-28 shutdown now
+ssh root@hadoop-29 shutdown now
+ssh root@hadoop-32 shutdown now
