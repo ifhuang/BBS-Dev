5f97e13d92df88bd52bc319c118d81d9ee1a87a4
If <ifhuang91@gmail.com>
2013-12-06 16:12:44 +0800

    backup hbase conf

---
+++ conf/hbase-0.90.4/copy-conf.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+scp -r * hadoop-4:/usr/yifu/hbase-0.90.4/conf/
+scp -r * hadoop-5:/usr/yifu/hbase-0.90.4/conf/
+scp -r * hadoop-6:/usr/yifu/hbase-0.90.4/conf/
+scp -r * hadoop-7:/usr/yifu/hbase-0.90.4/conf/
+scp -r * hadoop-9:/usr/yifu/hbase-0.90.4/conf/
+scp -r * hadoop-10:/usr/yifu/hbase-0.90.4/conf/
+scp -r * hadoop-11:/usr/yifu/hbase-0.90.4/conf/
+scp -r * hadoop-12:/usr/yifu/hbase-0.90.4/conf/
+scp -r * hadoop-13:/usr/yifu/hbase-0.90.4/conf/
+scp -r * hadoop-14:/usr/yifu/hbase-0.90.4/conf/
+scp -r * hadoop-15:/usr/yifu/hbase-0.90.4/conf/
+scp -r * hadoop-16:/usr/yifu/hbase-0.90.4/conf/
+scp -r * hadoop-19:/usr/yifu/hbase-0.90.4/conf/
+scp -r * hadoop-20:/usr/yifu/hbase-0.90.4/conf/
+scp -r * hadoop-21:/usr/yifu/hbase-0.90.4/conf/
+scp -r * hadoop-22:/usr/yifu/hbase-0.90.4/conf/
+scp -r * hadoop-23:/usr/yifu/hbase-0.90.4/conf/
+scp -r * hadoop-24:/usr/yifu/hbase-0.90.4/conf/
+scp -r * hadoop-25:/usr/yifu/hbase-0.90.4/conf/
+scp -r * hadoop-26:/usr/yifu/hbase-0.90.4/conf/
+scp -r * hadoop-27:/usr/yifu/hbase-0.90.4/conf/
+scp -r * hadoop-28:/usr/yifu/hbase-0.90.4/conf/
+scp -r * hadoop-29:/usr/yifu/hbase-0.90.4/conf/
+scp -r * hadoop-32:/usr/yifu/hbase-0.90.4/conf/
---
+++ conf/hbase-0.90.4/replace-hadoop-core.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+ssh hadoop@hadoop-4 rm /usr/yifu/hbase-0.90.4/lib/hadoop-core-0.20-append-r1056497.jar
+ssh hadoop@hadoop-4 cp /usr/yifu/hadoop-1.2.1/hadoop-core-1.2.1.jar /usr/yifu/hbase-0.90.4/lib/
+ssh hadoop@hadoop-4 chmod 777 /usr/yifu/hbase-0.90.4/lib/hadoop-core-1.2.1.jar
+ssh hadoop@hadoop-5 rm /usr/yifu/hbase-0.90.4/lib/hadoop-core-0.20-append-r1056497.jar
+ssh hadoop@hadoop-5 cp /usr/yifu/hadoop-1.2.1/hadoop-core-1.2.1.jar /usr/yifu/hbase-0.90.4/lib/
+ssh hadoop@hadoop-5 chmod 777 /usr/yifu/hbase-0.90.4/lib/hadoop-core-1.2.1.jar
+ssh hadoop@hadoop-6 rm /usr/yifu/hbase-0.90.4/lib/hadoop-core-0.20-append-r1056497.jar
+ssh hadoop@hadoop-6 cp /usr/yifu/hadoop-1.2.1/hadoop-core-1.2.1.jar /usr/yifu/hbase-0.90.4/lib/
+ssh hadoop@hadoop-6 chmod 777 /usr/yifu/hbase-0.90.4/lib/hadoop-core-1.2.1.jar
+ssh hadoop@hadoop-7 rm /usr/yifu/hbase-0.90.4/lib/hadoop-core-0.20-append-r1056497.jar
+ssh hadoop@hadoop-7 cp /usr/yifu/hadoop-1.2.1/hadoop-core-1.2.1.jar /usr/yifu/hbase-0.90.4/lib/
+ssh hadoop@hadoop-7 chmod 777 /usr/yifu/hbase-0.90.4/lib/hadoop-core-1.2.1.jar
+ssh hadoop@hadoop-9 rm /usr/yifu/hbase-0.90.4/lib/hadoop-core-0.20-append-r1056497.jar
+ssh hadoop@hadoop-9 cp /usr/yifu/hadoop-1.2.1/hadoop-core-1.2.1.jar /usr/yifu/hbase-0.90.4/lib/
+ssh hadoop@hadoop-9 chmod 777 /usr/yifu/hbase-0.90.4/lib/hadoop-core-1.2.1.jar
+ssh hadoop@hadoop-10 rm /usr/yifu/hbase-0.90.4/lib/hadoop-core-0.20-append-r1056497.jar
+ssh hadoop@hadoop-10 cp /usr/yifu/hadoop-1.2.1/hadoop-core-1.2.1.jar /usr/yifu/hbase-0.90.4/lib/
+ssh hadoop@hadoop-10 chmod 777 /usr/yifu/hbase-0.90.4/lib/hadoop-core-1.2.1.jar
+ssh hadoop@hadoop-11 rm /usr/yifu/hbase-0.90.4/lib/hadoop-core-0.20-append-r1056497.jar
+ssh hadoop@hadoop-11 cp /usr/yifu/hadoop-1.2.1/hadoop-core-1.2.1.jar /usr/yifu/hbase-0.90.4/lib/
+ssh hadoop@hadoop-11 chmod 777 /usr/yifu/hbase-0.90.4/lib/hadoop-core-1.2.1.jar
+ssh hadoop@hadoop-12 rm /usr/yifu/hbase-0.90.4/lib/hadoop-core-0.20-append-r1056497.jar
+ssh hadoop@hadoop-12 cp /usr/yifu/hadoop-1.2.1/hadoop-core-1.2.1.jar /usr/yifu/hbase-0.90.4/lib/
+ssh hadoop@hadoop-12 chmod 777 /usr/yifu/hbase-0.90.4/lib/hadoop-core-1.2.1.jar
+ssh hadoop@hadoop-13 rm /usr/yifu/hbase-0.90.4/lib/hadoop-core-0.20-append-r1056497.jar
+ssh hadoop@hadoop-13 cp /usr/yifu/hadoop-1.2.1/hadoop-core-1.2.1.jar /usr/yifu/hbase-0.90.4/lib/
+ssh hadoop@hadoop-13 chmod 777 /usr/yifu/hbase-0.90.4/lib/hadoop-core-1.2.1.jar
+ssh hadoop@hadoop-14 rm /usr/yifu/hbase-0.90.4/lib/hadoop-core-0.20-append-r1056497.jar
+ssh hadoop@hadoop-14 cp /usr/yifu/hadoop-1.2.1/hadoop-core-1.2.1.jar /usr/yifu/hbase-0.90.4/lib/
+ssh hadoop@hadoop-14 chmod 777 /usr/yifu/hbase-0.90.4/lib/hadoop-core-1.2.1.jar
+ssh hadoop@hadoop-15 rm /usr/yifu/hbase-0.90.4/lib/hadoop-core-0.20-append-r1056497.jar
+ssh hadoop@hadoop-15 cp /usr/yifu/hadoop-1.2.1/hadoop-core-1.2.1.jar /usr/yifu/hbase-0.90.4/lib/
+ssh hadoop@hadoop-15 chmod 777 /usr/yifu/hbase-0.90.4/lib/hadoop-core-1.2.1.jar
+ssh hadoop@hadoop-16 rm /usr/yifu/hbase-0.90.4/lib/hadoop-core-0.20-append-r1056497.jar
+ssh hadoop@hadoop-16 cp /usr/yifu/hadoop-1.2.1/hadoop-core-1.2.1.jar /usr/yifu/hbase-0.90.4/lib/
+ssh hadoop@hadoop-16 chmod 777 /usr/yifu/hbase-0.90.4/lib/hadoop-core-1.2.1.jar
+ssh hadoop@hadoop-19 rm /usr/yifu/hbase-0.90.4/lib/hadoop-core-0.20-append-r1056497.jar
+ssh hadoop@hadoop-19 cp /usr/yifu/hadoop-1.2.1/hadoop-core-1.2.1.jar /usr/yifu/hbase-0.90.4/lib/
+ssh hadoop@hadoop-19 chmod 777 /usr/yifu/hbase-0.90.4/lib/hadoop-core-1.2.1.jar
+ssh hadoop@hadoop-20 rm /usr/yifu/hbase-0.90.4/lib/hadoop-core-0.20-append-r1056497.jar
+ssh hadoop@hadoop-20 cp /usr/yifu/hadoop-1.2.1/hadoop-core-1.2.1.jar /usr/yifu/hbase-0.90.4/lib/
+ssh hadoop@hadoop-20 chmod 777 /usr/yifu/hbase-0.90.4/lib/hadoop-core-1.2.1.jar
+ssh hadoop@hadoop-21 rm /usr/yifu/hbase-0.90.4/lib/hadoop-core-0.20-append-r1056497.jar
+ssh hadoop@hadoop-21 cp /usr/yifu/hadoop-1.2.1/hadoop-core-1.2.1.jar /usr/yifu/hbase-0.90.4/lib/
+ssh hadoop@hadoop-21 chmod 777 /usr/yifu/hbase-0.90.4/lib/hadoop-core-1.2.1.jar
+ssh hadoop@hadoop-22 rm /usr/yifu/hbase-0.90.4/lib/hadoop-core-0.20-append-r1056497.jar
+ssh hadoop@hadoop-22 cp /usr/yifu/hadoop-1.2.1/hadoop-core-1.2.1.jar /usr/yifu/hbase-0.90.4/lib/
+ssh hadoop@hadoop-22 chmod 777 /usr/yifu/hbase-0.90.4/lib/hadoop-core-1.2.1.jar
+ssh hadoop@hadoop-23 rm /usr/yifu/hbase-0.90.4/lib/hadoop-core-0.20-append-r1056497.jar
+ssh hadoop@hadoop-23 cp /usr/yifu/hadoop-1.2.1/hadoop-core-1.2.1.jar /usr/yifu/hbase-0.90.4/lib/
+ssh hadoop@hadoop-23 chmod 777 /usr/yifu/hbase-0.90.4/lib/hadoop-core-1.2.1.jar
+ssh hadoop@hadoop-24 rm /usr/yifu/hbase-0.90.4/lib/hadoop-core-0.20-append-r1056497.jar
+ssh hadoop@hadoop-24 cp /usr/yifu/hadoop-1.2.1/hadoop-core-1.2.1.jar /usr/yifu/hbase-0.90.4/lib/
+ssh hadoop@hadoop-24 chmod 777 /usr/yifu/hbase-0.90.4/lib/hadoop-core-1.2.1.jar
+ssh hadoop@hadoop-25 rm /usr/yifu/hbase-0.90.4/lib/hadoop-core-0.20-append-r1056497.jar
+ssh hadoop@hadoop-25 cp /usr/yifu/hadoop-1.2.1/hadoop-core-1.2.1.jar /usr/yifu/hbase-0.90.4/lib/
+ssh hadoop@hadoop-25 chmod 777 /usr/yifu/hbase-0.90.4/lib/hadoop-core-1.2.1.jar
+ssh hadoop@hadoop-26 rm /usr/yifu/hbase-0.90.4/lib/hadoop-core-0.20-append-r1056497.jar
+ssh hadoop@hadoop-26 cp /usr/yifu/hadoop-1.2.1/hadoop-core-1.2.1.jar /usr/yifu/hbase-0.90.4/lib/
+ssh hadoop@hadoop-26 chmod 777 /usr/yifu/hbase-0.90.4/lib/hadoop-core-1.2.1.jar
+ssh hadoop@hadoop-27 rm /usr/yifu/hbase-0.90.4/lib/hadoop-core-0.20-append-r1056497.jar
+ssh hadoop@hadoop-27 cp /usr/yifu/hadoop-1.2.1/hadoop-core-1.2.1.jar /usr/yifu/hbase-0.90.4/lib/
+ssh hadoop@hadoop-27 chmod 777 /usr/yifu/hbase-0.90.4/lib/hadoop-core-1.2.1.jar
+ssh hadoop@hadoop-28 rm /usr/yifu/hbase-0.90.4/lib/hadoop-core-0.20-append-r1056497.jar
+ssh hadoop@hadoop-28 cp /usr/yifu/hadoop-1.2.1/hadoop-core-1.2.1.jar /usr/yifu/hbase-0.90.4/lib/
+ssh hadoop@hadoop-28 chmod 777 /usr/yifu/hbase-0.90.4/lib/hadoop-core-1.2.1.jar
+ssh hadoop@hadoop-29 rm /usr/yifu/hbase-0.90.4/lib/hadoop-core-0.20-append-r1056497.jar
+ssh hadoop@hadoop-29 cp /usr/yifu/hadoop-1.2.1/hadoop-core-1.2.1.jar /usr/yifu/hbase-0.90.4/lib/
+ssh hadoop@hadoop-29 chmod 777 /usr/yifu/hbase-0.90.4/lib/hadoop-core-1.2.1.jar
+ssh hadoop@hadoop-32 rm /usr/yifu/hbase-0.90.4/lib/hadoop-core-0.20-append-r1056497.jar
+ssh hadoop@hadoop-32 cp /usr/yifu/hadoop-1.2.1/hadoop-core-1.2.1.jar /usr/yifu/hbase-0.90.4/lib/
+ssh hadoop@hadoop-32 chmod 777 /usr/yifu/hbase-0.90.4/lib/hadoop-core-1.2.1.jar
