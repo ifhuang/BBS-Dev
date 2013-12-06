6c51210677009059fdd181293bd7522e3a270ac7
If <ifhuang91@gmail.com>
2013-12-07 01:11:46 +0800

    hadoop/hbase conf improve

---
+++ conf/hbase-0.90.4/time-sync.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+ssh root@hadoop-4 ntpdate time.nist.gov
+ssh root@hadoop-5 ntpdate time.nist.gov
+ssh root@hadoop-6 ntpdate time.nist.gov
+ssh root@hadoop-7 ntpdate time.nist.gov
+ssh root@hadoop-9 ntpdate time.nist.gov
+ssh root@hadoop-10 ntpdate time.nist.gov
+ssh root@hadoop-11 ntpdate time.nist.gov
+ssh root@hadoop-12 ntpdate time.nist.gov
+ssh root@hadoop-13 ntpdate time.nist.gov
+ssh root@hadoop-14 ntpdate time.nist.gov
+ssh root@hadoop-15 ntpdate time.nist.gov
+ssh root@hadoop-16 ntpdate time.nist.gov
+ssh root@hadoop-19 ntpdate time.nist.gov
+ssh root@hadoop-20 ntpdate time.nist.gov
+ssh root@hadoop-21 ntpdate time.nist.gov
+ssh root@hadoop-22 ntpdate time.nist.gov
+ssh root@hadoop-23 ntpdate time.nist.gov
+ssh root@hadoop-24 ntpdate time.nist.gov
+ssh root@hadoop-25 ntpdate time.nist.gov
+ssh root@hadoop-26 ntpdate time.nist.gov
+ssh root@hadoop-27 ntpdate time.nist.gov
+ssh root@hadoop-28 ntpdate time.nist.gov
+ssh root@hadoop-29 ntpdate time.nist.gov
+ssh root@hadoop-32 ntpdate time.nist.gov
