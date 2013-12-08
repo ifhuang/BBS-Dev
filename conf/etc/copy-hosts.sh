3a91375b83d434cd08b100f4c7251aa54a4bfa65
If <ifhuang91@gmail.com>
2013-12-09 02:20:29 +0800

    fix hbase regionserver listen bug

---
+++ conf/etc/copy-hosts.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+sudo scp /etc/hosts hadoop-4:/etc/hosts
+sudo scp /etc/hosts hadoop-5:/etc/hosts
+sudo scp /etc/hosts hadoop-6:/etc/hosts
+sudo scp /etc/hosts hadoop-7:/etc/hosts
+sudo scp /etc/hosts hadoop-9:/etc/hosts
+sudo scp /etc/hosts hadoop-10:/etc/hosts
+sudo scp /etc/hosts hadoop-11:/etc/hosts
+sudo scp /etc/hosts hadoop-12:/etc/hosts
+sudo scp /etc/hosts hadoop-13:/etc/hosts
+sudo scp /etc/hosts hadoop-14:/etc/hosts
+sudo scp /etc/hosts hadoop-15:/etc/hosts
+sudo scp /etc/hosts hadoop-16:/etc/hosts
+sudo scp /etc/hosts hadoop-19:/etc/hosts
+sudo scp /etc/hosts hadoop-20:/etc/hosts
+sudo scp /etc/hosts hadoop-21:/etc/hosts
+sudo scp /etc/hosts hadoop-22:/etc/hosts
+sudo scp /etc/hosts hadoop-23:/etc/hosts
+sudo scp /etc/hosts hadoop-24:/etc/hosts
+sudo scp /etc/hosts hadoop-25:/etc/hosts
+sudo scp /etc/hosts hadoop-26:/etc/hosts
+sudo scp /etc/hosts hadoop-27:/etc/hosts
+sudo scp /etc/hosts hadoop-28:/etc/hosts
+sudo scp /etc/hosts hadoop-29:/etc/hosts
+sudo scp /etc/hosts hadoop-32:/etc/hosts
