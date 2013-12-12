9a6a95a6959a849c578ad8e33fdb12871ae829e5
If <ifhuang91@gmail.com>
2013-12-12 20:00:18 +0800

    improve ui

--- bbs/app/controllers/home_controller.rb
+++ bbs/app/controllers/home_controller.rb
@@ -2,12 +2,12 @@ require 'net/http'
 class HomeController < ApplicationController
   def index
   	if params[:query].nil? || params[:query]==""
-  		response = Net::HTTP.get_response(URI.parse("http://hadoop-2:8983/solr/collection1/select?q=content:*&wt=json&indent=true"))
+  		@docs = JSON.parse("{}")
   	else
-  		response = Net::HTTP.get_response(URI.parse(URI.encode("http://hadoop-2:8983/solr/collection1/select?q=content:"<<params[:query]<<"&wt=json&indent=true")))
-  	end
-  	body = response.body
-  	body_utf8 = body.force_encoding("UTF-8")
-  	@docs = JSON.parse(body_utf8)["response"]["docs"]
+  		response = Net::HTTP.get_response(URI.parse(URI.encode("http://hadoop-2:8983/solr/collection1/select?q=content:"<<params[:query]<<"&rows=100&wt=json&indent=true")))
+      body = response.body
+      body_utf8 = body.force_encoding("UTF-8")
+      @docs = JSON.parse(body_utf8)["response"]["docs"]
+    end
   end
 end
