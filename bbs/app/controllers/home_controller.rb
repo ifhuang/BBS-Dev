4817a8a814c792d123050a4472218c17aa57772f
If <ifhuang91@gmail.com>
2014-02-25 18:13:29 +0800

    update

--- bbs/app/controllers/home_controller.rb
+++ bbs/app/controllers/home_controller.rb
@@ -1,10 +1,14 @@
 require 'net/http'
 class HomeController < ApplicationController
   def index
-  	if params[:query].nil? || params[:query]==""
+  	if params[:query].nil? || params[:query].strip==""
   		@docs = JSON.parse("{}")
   	else
-  		response = Net::HTTP.get_response(URI.parse(URI.encode("http://hadoop-2:8983/solr/collection1/select?q=content:"<<params[:query]<<"&rows=100&wt=json&indent=true")))
+      andquery = ""
+      params[:query].split(' ').each do |word|
+        andquery<<"%2B%22"<<URI.encode(word)<<"%22"
+      end
+  		response = Net::HTTP.get_response(URI.parse("http://hadoop-2:8983/solr/collection1/select?q="<<andquery<<"&rows=100&wt=json"))
       body = response.body
       body_utf8 = body.force_encoding("UTF-8")
       @docs = JSON.parse(body_utf8)["response"]["docs"]
